b685c3041480ef3b52af7305cf8a766056cf2281
Tatsukikun999 <59278386+Tatsukikun999@users.noreply.github.com>
2020-01-21 15:51:14 +0700

    Update lab7_3.cpp

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,1 +1,32 @@
-//status = "ONE-NIGHT-STAND";
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,h,e;
+    cout<<"Enter your age: ";
+    cin>>a;
+    if(a<=20){
+        cout<<"Enter yout height: ";
+        cin>>h;
+        if(h<160) cout<<"UNFRIEND";
+        else if(h<175) cout<<"FRIEND";
+        else cout<<"Enter your estate :";
+        cin>>e;
+        if(e>69000000){
+            cout<<"MARRIED";
+        }else{
+            cout<<"ONE NIGHT STAND";
+        }
+        
+    }else{
+        if(a<30){
+            cout<<"Enter your Estate: ";
+            cin>>e;
+            if(e>=10000000) cout<<"BEST FRIEND";
+            else cout<<"UNFREIND";
+        }else{
+            cout<<"UNFRIEND";
+        }
+    }
+}
+
